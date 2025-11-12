abf93413a5e5b9464327c6fff687615942984c28
JaehoonSong12 <manual20151276@gmail.com>
2025-11-12 10:38:15 -0500

    Normal Execution commit: 2025-11-12_10-38-14 updated by Jaehoon

--- docs/sample_jslawgroup2/js/hi.js
+++
@@ -1,9 +0,0 @@
-// event-driven programming
-import * as Util from '../core-module.js';
-
-export default function main() {
-    // Util.initialSetup();
-    // Util.stylePage();
-    // Util.addCenteringFunctionality();
-    Util.enableButtonRedirect('btn-redirect');
-}
